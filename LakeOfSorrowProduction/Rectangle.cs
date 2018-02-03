b16183f9be990d3d9fe61e490b9eb1c907de9d99
Code0N <redeemer.of.souls@yandex.ru>
2018-02-03 19:35:10 +0300

    Без эксепшенов

--- LakeOfSorrowProduction/Rectangle.cs
+++ LakeOfSorrowProduction/Rectangle.cs
@@ -7,19 +7,33 @@ namespace LakeOfSorrowProduction
 {
     public class Rectangle
     {
-        public Rectangle(double[] x_param, double[] y_param) //Торвальдс покарает меня за этот код
+        public Rectangle(double[] x_param, double[] y_param)
         {
-            x = x_param;
-            y = y_param;
+            SetVertices(x_param, y_param);
         }
 
-        public void SetVertices(double[] x_param, double[] y_param) //Говнокооод, Говнокоооод
+        public void SetVertices(double[] x_param, double[] y_param) //Торвальдс покарает меня за этот код
         {
-            x = x_param;
-            y = y_param;
+            this.x = x_param;
+            this.y = y_param;
+            this.a = this.x[1] - this.x[0];
+            this.b = this.y[1] - this.y[2];
+        }
+
+        public double Diagonal()
+        {
+            if ((x.Length != 4) || (y.Length != 4))
+            {
+                throw new ArgumentException("Неверно заданы вершины, это не прямоугольник");
+            }
+
+
+
+            return Math.Sqrt(Math.Pow(this.b, 2) + Math.Pow(this.a, 2)); //Эксепшны сверху
         }
 
         private double[] x;
         private double[] y;
+        double a, b;
     }
 }

bfcf2782652f9b56b95c9bc0991abcc8468d2c97
Code0N <redeemer.of.souls@yandex.ru>
2018-02-03 18:00:20 +0300

    Базовая реализация Rectangle

---
+++ LakeOfSorrowProduction/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LakeOfSorrowProduction
+{
+    public class Rectangle
+    {
+        public Rectangle(double[] x_param, double[] y_param) //Торвальдс покарает меня за этот код
+        {
+            x = x_param;
+            y = y_param;
+        }
+
+        public void SetVertices(double[] x_param, double[] y_param) //Говнокооод, Говнокоооод
+        {
+            x = x_param;
+            y = y_param;
+        }
+
+        private double[] x;
+        private double[] y;
+    }
+}

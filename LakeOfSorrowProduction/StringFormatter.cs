aef903fd8d21f6ce2434c6ba519a8433154ef639
Code0N <redeemer.of.souls@yandex.ru>
2018-02-04 11:54:45 +0300

    Реализован метод ShortFileString класса StringFormatter

--- LakeOfSorrowProduction/StringFormatter.cs
+++ LakeOfSorrowProduction/StringFormatter.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace LakeOfSorrowProduction
 {
-    class StringFormatter
+    public class StringFormatter
     {
+        string ShortFileString(string path)
+        {
+            if (path == String.Empty)
+                return path;
+            if (path == null)
+                throw new NullReferenceException("String is Null");
+
+            return (Path.ChangeExtension(Path.GetFileName(path), ".TMP")).ToUpperInvariant();
+        }
     }
 }
